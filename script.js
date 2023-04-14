e45fcb30f190eda5193425251a7eb4e0a8b52f7f
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-04-14 17:32:52 +0300

    add random quote in the card by btn

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const quotes = []
 fetch('quotes.txt')
   .then(response => response.text())
   .then(text => text.split('\n').map(line => quotes.push(line)));
+
+
+const quoteEl = document.getElementById("quote");
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+    let rnd = Math.floor(Math.random() * 94);
+    quoteEl.textContent = quotes[rnd];
+});
